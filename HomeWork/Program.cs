e49136debe29fa49f37445bf2ea9efb2f4ae4acd
Danya <danya.sydortsov@tech-stack.io>
2021-12-15 18:14:53 +0200

    First commit

---
+++ HomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace HomeWork
+{
+    class Work
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            int[] arrayRandom = new int[5];
+            Console.WriteLine("Array random number ");
+
+            for (int i = 0; i < arrayRandom.Length; i++)
+            {
+                arrayRandom[i] = random.Next(0, 100);
+                Console.Write(arrayRandom[i] + " ");
+            }
+            Console.WriteLine("\nEven numbers from the array ");
+
+            for (int i = 0; i < arrayRandom.Length; i++)
+            {
+                if (arrayRandom[i] % 2 == 0)
+                {
+                    Console.Write(arrayRandom[i] + " ");
+                }
+            }
+            Console.WriteLine("\nSort array");
+            int temp;
+            for (int i = 0;i < arrayRandom.Length - 1; i++)
+                for (int j = i + 1;j < arrayRandom.Length; j++)
+                    if (arrayRandom[i] < arrayRandom[j])
+                    {
+                        temp = arrayRandom[i];
+                        arrayRandom[i] = arrayRandom[j];
+                        arrayRandom[j] = temp;
+                    }
+            foreach (int value in arrayRandom)
+            {
+                Console.Write(value + " ");
+            }
+
+        }
+    }
+}
